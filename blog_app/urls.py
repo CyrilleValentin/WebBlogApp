3f844a373b950cf4c2c507877cd7f0f6e8e44482
cyrille <cyrillekyrillos6@gmail.com>
2024-01-05 22:29:54 +0000

    creation of post detail page  and urls

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -3,4 +3,5 @@ from blog_app import views
 
 urlpatterns = [
     path('home/', views.home, name='home'),
+    path('post_detail/', views.post_detail, name='post_detail'),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -4,3 +4,6 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def home(request):
     return render(request, 'home.html')
+@login_required
+def post_detail(request):
+    return render(request, 'post_detail.html')
