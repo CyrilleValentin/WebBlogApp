64a17f7982f4b2e7346449a9c00833f9f1852109
cyrille <cyrillekyrillos6@gmail.com>
2024-01-06 22:37:47 +0000

    post save function and urls

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -4,4 +4,6 @@ from blog_app import views
 urlpatterns = [
     path('home/', views.home, name='home'),
     path('post_detail/', views.post_detail, name='post_detail'),
+    path('new_post/', views.new_post, name='new_post'),
+    path('create/', views.create_blog, name='create_blog'),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
+
+from blog_app.models import Blog
 # Create your views here.
 @login_required
 def home(request):
@@ -7,3 +9,17 @@ def home(request):
 @login_required
 def post_detail(request):
     return render(request, 'post_detail.html')
+@login_required
+def new_post(request):
+    return render(request, 'new_post.html')
+def create_blog(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        image = request.FILES.get('image')
+        user = request.user
+        blog = Blog(title=title, content=content, image=image, user=user)
+        blog.save()
+       ## return redirect('list_blogs')  
+
+    return render(request, 'new_post.html')
